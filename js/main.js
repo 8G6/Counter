534939a89be88b73c9de567afbef0a73eed36fa8
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2021-08-09 22:34:39 +0530

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 let v=0
 $=(a)=>document.querySelector(a)
-change = () => {$('#screen').value=v}
-add    = () => {v+=1;change()}
-sub    = () => {v-=1;change()}
-reset  = () => {v=0;change()}
+change = () => {$('#screen').innerHTML=v.toString()}
+add    = () => { v+=1; change()}
+reset  = () => { v=0;  change()}
