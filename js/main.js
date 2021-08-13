09e6724dfc8e364bca688cfdd4f784ccadc3dd43
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2021-08-13 18:05:37 +0530

    css

--- js/main.js
+++ js/main.js
@@ -3,4 +3,5 @@ let int;
 $=(a)=>document.querySelector(a)
 change = ()         => {$('#screen').innerHTML=v.toString()}
 add    = ()         => { v+=1; change()}
+sub    = ()         => { v-=1; change()}
 reset  = ()         => { v=0;  change();clearInterval(int)}
