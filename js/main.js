d2aaf57f850eb90e29ba61cd4af0a39482da7af0
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2021-08-13 16:34:45 +0530

    css-update

--- js/main.js
+++ js/main.js
@@ -4,4 +4,3 @@ $=(a)=>document.querySelector(a)
 change = ()         => {$('#screen').innerHTML=v.toString()}
 add    = ()         => { v+=1; change()}
 reset  = ()         => { v=0;  change();clearInterval(int)}
-auto   = (val=1000) => {clearInterval(int);int=setInterval(add,val)}
